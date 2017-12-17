73f1566ce1fe19a07ded5336a2d15aff03c2e5ce
root <root@ICB12.localdomain>
2017-12-17 17:19:28 +0500

    version 1

--- queue.js
+++ queue.js
@@ -1,4 +1,29 @@
-function Queue() {
+// // function Queue() {
 
-  this.storage = [];
-};
+// //   this.storage = [];
+// // };
+
+// class Queue{
+//   constructor(){ this.storage = [];}
+//   add(value){this.storage.push(value)}
+//   remove(){return this.storage.shift()}
+//   size(){return this.storage.length}
+// }
+
+// // class Stack{
+// //   constructor(){ this.storage = [];}
+// //   add(value){this.storage.push(value)}
+// //   remove(){return this.storage.pop()}
+// //   size(){return this.storage.length}
+// // }
+
+// function Stack(){
+// var storage=[];
+
+// this.add= function(value){storage.push(value)};
+// this.remove=function(){return storage.pop()};
+// this.size=function(){return storage.length};
+// return this;
+// }
+
+
